3f9df5bcd2b544972e365c4569c23b925883686a
Kashif Ali <kashifali0969082@gmail.com>
2023-11-27 01:51:24 +0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+import Nav from './Components/nav';
+import Hero from './Components/hero';
+function App() {
+  return (
+    <div >
+     <Nav/>
+     <Hero/>
+    </div>
+  );
+}
+
+export default App;
---
+++ hero.jsx
@@ -0,0 +1,29 @@
+const Hero=()=>{
+return(
+<main className="hero container">
+    <div className="hero_content">
+        <h1>
+        YOUR FEET DESERVES THE BEST
+        </h1>
+<p>
+    YOUR FEET DESERVE THE BEST AND WERE HERE TO HELP  YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WERE HERE TO HELP YOU WITH OUR SHOES 
+</p>
+<div className="hero_button">
+    <button>Shop now</button>
+    <button className="secondary_btn">Category</button>
+</div>
+<div className="shopping">
+<p>Also Availble on:</p>
+<div className="brand_icons">
+    <img src="/images/amazon.png" alt="mazon_logo" />
+    <img src="/images/flipkart.png" alt="mazon_logo" />
+    </div>
+</div>
+    </div>
+<div className="hero_image">
+    <img src="/images/shoe_image.png" alt="hero_img" />
+</div>
+</main>
+);
+}
+export default Hero;
